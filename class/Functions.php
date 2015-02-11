665b3b2852697020daece4253c0dc518d350d78d
Harrison M. Mungai <harrymuhari@gmail.com>
2015-02-11 16:51:36 +0300

    Initial commit

---
+++ class/Functions.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'core/DBConnection.php';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Functions
+ *
+ * @author hmungai
+ */
+class Functions {
+    //put your code here
+}
