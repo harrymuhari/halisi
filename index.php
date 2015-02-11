c5e3c5a11d785b44c5e0938e0f5aa96544741976
Harrison M. Mungai <harrymuhari@gmail.com>
2015-02-11 16:12:12 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Halisi - Musings of a Kenyan techie</title>
+        <meta name="keywords" content="technology, programming, tutorials, startups, etc.">
+        <meta name="description" content="All about the technology ecosystems in and around Kenya">
+        <meta name="auhor" content="VA">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
