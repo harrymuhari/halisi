8adc8e5246a1753d15318f4c3de06d27ba4dd791
Harrison M. Mungai <harrymuhari@gmail.com>
2015-03-05 18:31:30 +0300

    Added log() function

--- class/Functions.php
+++ class/Functions.php
@@ -8,10 +8,36 @@ require_once 'core/DBConnection.php';
  */
 
 /**
- * Description of Functions
+ * Utility class
  *
  * @author hmungai
  */
 class Functions {
-    //put your code here
+
+    /**
+     * Assists in logging the variuos system events to the specified folder and file
+     *
+     * @param string $folder_name the name of the log folder
+     * @param string $file_name the name of the log file
+     * @param string $message the message to be logged
+     * @return void
+     */
+    public function log($message, $folder_name = null, $file_name = null) {
+        if (PHP_OS == "Linux") {
+            $location = "/home/Halisi_Logs/";
+        } else {
+            $location = "C:\\Halisi_Logs\\";
+        }
+
+        $folder_name = $folder_name . '-' . date("Y-m-d") . DIRECTORY_SEPARATOR;
+        $path = $location . $folder_name;
+        if (!is_dir($path)) {
+            echo $path;
+            mkdir($path, 0777);
+        }
+
+        $file_name = $path . DIRECTORY_SEPARATOR . date("Y-m-d") . '.log';
+        file_put_contents($file_name, date("Y-m-d H:i:s") . " - " . $message . PHP_EOL, FILE_APPEND | LOCK_EX);
+    }
+
 }
