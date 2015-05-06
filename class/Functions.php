7db2ea6001c454a7110d1b739206e931fcb67e08
Harrison M. Mungai <harrymuhari@gmail.com>
2015-05-06 13:07:40 +0300

    Modified logging function to include class and method names for easier message tracing

--- class/Functions.php
+++ class/Functions.php
@@ -17,12 +17,15 @@ class Functions {
     /**
      * Assists in logging the variuos system events to the specified folder and file
      *
+     * @param string $className the name of the originating class
+     * @param string $methodName the name of  the originating method
+     * @param string $message the message to be logged
      * @param string $folder_name the name of the log folder
      * @param string $file_name the name of the log file
-     * @param string $message the message to be logged
+     * 
      * @return void
      */
-    public function log($message, $folder_name = null, $file_name = null) {
+    public function log($className, $methodName, $message, $folder_name = null, $file_name = null) {
         if (PHP_OS == "Linux") {
             $location = "/home/Halisi_Logs/";
         } else {
@@ -35,8 +38,8 @@ class Functions {
             mkdir($path, 0777);
         }
 
-        $file_name = $path . DIRECTORY_SEPARATOR . $file_name . '-' . date("Y-m-d") . '.log';
-        file_put_contents($file_name, date("Y-m-d H:i:s") . " - " . $message . PHP_EOL, FILE_APPEND | LOCK_EX);
+        $file_name = $path . DIRECTORY_SEPARATOR . $file_name . '-' . date("Y-m-d H") . '.log';
+        file_put_contents($file_name, date("Y-m-d H:i:s") . " - " . $className . '->' . $methodName . ': ' . $message . PHP_EOL, FILE_APPEND | LOCK_EX);
     }
 
 }
